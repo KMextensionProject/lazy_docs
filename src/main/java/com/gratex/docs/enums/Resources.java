71ca210354fc09b9ae6ddbfdc96497c9e234ff9a
Martin Krajcovic <mar.krajc@gmail.com>
2021-11-17 21:50:14 +0100

    added Resources.java containing resource location paths

--- src/main/java/com/gratex/docs/enums/Resources.java
+++ src/main/java/com/gratex/docs/enums/Resources.java
@@ -2,6 +2,8 @@ package com.gratex.docs.enums;
 
 public class Resources {
 
-	public static final String VYBER_SPOSOBU_STRAVOVANIA = "/docs/Vyber_sposobu_stravovania.docx";
+	public static final String MESSAGES_MAP = "/messages.properties";
 
+	public static final String VYBER_SPOSOBU_STRAVOVANIA = "/docs/Vyber_sposobu_stravovania.docx";
+	
 }
