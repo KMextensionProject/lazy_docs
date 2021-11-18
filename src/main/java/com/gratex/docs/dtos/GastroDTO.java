0b12902ad18c0318a00428f20065a684ce407ee4
Martin Krajcovic <mar.krajc@gmail.com>
2021-11-18 20:14:28 +0100

    added option attribute into GastroDTO and repositioned options marks in template

--- src/main/java/com/gratex/docs/dtos/GastroDTO.java
+++ src/main/java/com/gratex/docs/dtos/GastroDTO.java
@@ -8,8 +8,8 @@ public class GastroDTO {
 	private String fullName;
 	private String address;
 	private String birthDate;
-	// TODO: add selection type
-	
+	private String option;
+
 	public String getFullName() {
 		return fullName;
 	}
@@ -34,11 +34,20 @@ public class GastroDTO {
 		this.birthDate = birthDate;
 	}
 
+	public String getOption() {
+		return this.option;
+	}
+
+	public void setOption(String option) {
+		this.option = option;
+	}
+
 	public Map<String, String> getAsMap() {
 		Map<String, String> gastroMap = new HashMap<>();
 		gastroMap.put("fullName", this.fullName);
 		gastroMap.put("address", this.address);
 		gastroMap.put("birthDate", this.birthDate);
+		gastroMap.put("option", this.option);
 		return gastroMap;
 	}
 }
